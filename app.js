3b5e3467bd9b38d6ffc3492166d398ae6523c93b
Lucas-Wanat <lucasantoniowanat@gmail.com>
2023-06-07 18:03:50 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const hbs = require("hbs");
+
+const gerador = require('./gerador');
+
+app.get('/', (req, res) => {
+    res.render('index.hbs')
+})
+
+app.get('/excel', gerador.gerarXlsx)
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+})
---
+++ gerador.js
@@ -0,0 +1,69 @@
+const XlsxPopulate = require('xlsx-populate');
+
+module.exports = {
+    gerarXlsx(req, res) {
+        let rows = [
+            {
+                nome_fantasia: "Empresa 1",
+                razao_social: "Empresa 1 LTDA",
+                cnpj: "123456789",
+                endereco: "Rua 1",
+                numero: "123",
+                bairro: "Bairro 1",
+                cidade: "Cidade 1",
+            },
+            {
+                nome_fantasia: "Empresa 2",
+                razao_social: "Empresa 2 LTDA",
+                cnpj: "987654321",
+                endereco: "Rua 2",
+                numero: "321",
+                bairro: "Bairro 2",
+                cidade: "Cidade 2",
+            },
+            {
+                nome_fantasia: "Empresa 3",
+                razao_social: "Empresa 3 LTDA",
+                cnpj: "123123123",
+                endereco: "Rua 3",
+                numero: "456",
+                bairro: "Bairro 3",
+                cidade: "Cidade 3",
+            },
+            {
+                nome_fantasia: "Empresa 4",
+                razao_social: "Empresa 4 LTDA",
+                cnpj: "321321321",
+                endereco: "Rua 4",
+                numero: "654",
+                bairro: "Bairro 4",
+                cidade: "Cidade 4",
+            }
+        ];
+
+        let nomeValor = Object.keys(rows[0]);
+
+        XlsxPopulate.fromBlankAsync()
+            .then(workbook => {
+                const sheet = workbook.sheet("Sheet1");
+
+                nomeValor.forEach((nome, index) => {
+                    sheet.row(1).cell(index + 1).value(nome)
+                })
+
+                let linha = 0;
+                rows.forEach((row) => {
+                    let valores = Object.values(row);
+                    linha = linha + 1;
+                    valores.forEach((valor, i) => {
+                        sheet.row(linha).cell(i + 1).value(valor);
+                    })
+                })
+
+                return workbook.toFileAsync("./output.xlsx");
+            });
+        
+        res.download('output.xlsx');
+    }
+    
+}
